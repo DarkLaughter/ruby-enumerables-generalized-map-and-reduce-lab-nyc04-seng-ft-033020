0112a76b140fa5701a97ff4ddd4dee800a02b943
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-22 16:06:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,4 +13,17 @@ end
 
 
 def reduce(array, sv=nil)
+  if sv 
+    num1 = sv
+    i = 0
+  else num1 = array[0]
+    i = 1 
+  end 
+
+  while i < array.length 
+      sum = yield(num1, array[i])
+    i += 1
+  end
+  sum
 end
+
